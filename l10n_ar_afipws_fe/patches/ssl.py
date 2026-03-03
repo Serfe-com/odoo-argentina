da214caf4e7823c0e12838432f18e0f1338647be
Gerónimo Rivoira <geronimo@serfe.com>
2026-03-03 07:40:37 -0300

    fix: sync with other patch

--- l10n_ar_afipws_fe/patches/ssl.py
+++ l10n_ar_afipws_fe/patches/ssl.py
@@ -3,20 +3,23 @@ import sys
 
 try:
     import httplib2
+
+    # corregir temas de negociacion de SSL en algunas versiones de ubuntu:
     import platform
 
-    # Only patch if not already patched
-    if getattr(httplib2, "_patched_ssl_context", False) is False:
-        _orig_build_ssl_context = httplib2._build_ssl_context
+    monkey_patch = sys.version_info < (3, ) or httplib2._build_ssl_context.__module__ != "httplib2"
+    needs_patch = platform.system() == 'Linux' or sys.version_info > (3, 10)
+    if needs_patch and not monkey_patch:
+        _build_ssl_context = httplib2._build_ssl_context
 
-        def _patched_build_ssl_context(disable_ssl_certificate_validation, ca_certs):
-            ctx = _orig_build_ssl_context(disable_ssl_certificate_validation, ca_certs)
-            # reduce SSL security level to avoid "DH_KEY_TOO_SMALL"
-            ctx.set_ciphers("DEFAULT@SECLEVEL=1")
-            return ctx
+        def _build_ssl_context_new(*args, **kwargs):
+            context = _build_ssl_context(*args, **kwargs)
+            # fix ssl.SSLError: [SSL: DH_KEY_TOO_SMALL] dh key too small
+            context.set_ciphers("DEFAULT@SECLEVEL=1")
+            # context.set_ciphers("AES128-SHA")
+            return context
 
-        httplib2._build_ssl_context = _patched_build_ssl_context
-        httplib2._patched_ssl_context = True
+        httplib2._build_ssl_context = _build_ssl_context_new
 
 except ImportError:
     pass  # httplib2 not installed yet
